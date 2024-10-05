27d90ea8e52db9b7b4603fe39328a420984dc95b
Kenny King <16931002+kennykingdev@users.noreply.github.com>
2024-10-04 21:06:48 -0700

    clean up config file

--- hosts/edgar/configuration.nix
+++ hosts/edgar/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 let
@@ -13,8 +9,12 @@ in
       ./hardware-configuration.nix
     ];
 
-  # Kernel modules
-  boot.initrd.kernelModules = [ "amdgpu" ];
+  # Boot Options
+  boot = {
+    initrd.kernelModules = [ "amdgpu" ];
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+  };
 
   networking = {
     hostName = "edgar";
@@ -34,40 +34,30 @@ in
     EDITOR = "nvim";
   };
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = locale;
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = locale;
-    LC_IDENTIFICATION = locale;
-    LC_MEASUREMENT = locale;
-    LC_MONETARY = locale;
-    LC_NAME = locale;
-    LC_NUMERIC = locale;
-    LC_PAPER = locale;
-    LC_TELEPHONE = locale;
-    LC_TIME = locale;
+  i18n = {
+    defaultLocale = locale;
+    extraLocaleSettings = {
+      LC_ADDRESS = locale;
+      LC_IDENTIFICATION = locale;
+      LC_MEASUREMENT = locale;
+      LC_MONETARY = locale;
+      LC_NAME = locale;
+      LC_NUMERIC = locale;
+      LC_PAPER = locale;
+      LC_TELEPHONE = locale;
+      LC_TIME = locale;
+    };
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+  # GUI
+  services = {
+    displayManager.sddm.enable = true;
+    displayManager.sddm.wayland.enable = true;
+    desktopManager.plasma6.enable = true;
   };
 
   # Enable CUPS to print documents.
@@ -89,10 +79,6 @@ in
     #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.kenny = {
     isNormalUser = true;
     description = "Kenny King";
@@ -141,32 +127,29 @@ in
     vulkan-tools # used to verify Vulkan drivers are correctly set up
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   # List services that you want to enable:
-  services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "no";
-      #PasswordAuthentication = false;
-      #KbdInteractiveAuthentication = false;
+  services = {
+    openssh = {
+      enable = true;
+      settings = {
+        PermitRootLogin = "no";
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+      };
     };
+    fail2ban.enable = true;
   };
-  services.fail2ban.enable = true;
 
   # GPU Acceleration
-  # Enable accelerated OpenGL rendering through the Direct Rendering Interface for 32-bit programs such as Wine
-  hardware.graphics.enable32Bit = true;
-  # OpenCL driver
-  hardware.graphics.extraPackages = with pkgs; [
-    rocmPackages_5.clr.icd  # Must use version 5 with my old RX560 (or any Polaris card)
-  ];
+  hardware.graphics = {
+    # Enable accelerated OpenGL rendering through the Direct Rendering Interface
+    # for 32-bit programs such as Wine
+    enable32Bit = true;
+    # OpenCL driver
+    extraPackages = with pkgs; [
+      rocmPackages_5.clr.icd  # Must use version 5 with my old RX560 (or any Polaris card)
+    ];
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
